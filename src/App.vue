988be11f05d87a83d899c044fd8bdd0f05f7f14d
nevena-kolev <72552488+nevena-kolev@users.noreply.github.com>
2022-10-14 14:04:33 +0200

    updating js code with vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-    <HelloWorld msg="Have a nice weekend!" />
+    <h3>Example</h3>
+
+    <form @submit.prevent="addNew">
+      <label for="item">Enter new subject: </label>
+      <input v-model="newSubject" />
+      <button>Add subject</button>
+      <hr />
+    </form>
+
+    <ul>
+      <li v-for="item in subjectArray">
+        {{ item.content }} <button @click="deleteItem">Delete</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import { ref } from 'vue';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
+  setup() {
+    const newSubject = ref(''); // bindings
+    const subjectArray = ref([]);
+
+    function addNew() {
+      if (newSubject.value !== '') {
+        subjectArray.value.push({ content: newSubject.value });
+        newSubject.value = '';
+      }
+    }
+
+    function deleteItem() {
+      const found = subjectArray.value.find(
+        (element) => element == newSubject.value
+      );
+      console.log(found);
+      subjectArray.value.pop(found);
+    }
+
+    return {
+      newSubject,
+      subjectArray,
+      addNew,
+      deleteItem,
+    };
   },
 };
+
+// delete-button neben jeden eintrag
 </script>
 
 <style>
